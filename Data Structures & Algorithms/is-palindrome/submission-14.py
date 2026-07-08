aa9d155756c8c9deb8f8f007c7f25f808b73f362
Zubair Shibly <88159206+StevieShibly8@users.noreply.github.com>
2026-07-07 21:22:32 -0400

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        concat = ''.join(c.lower() for c in s if c.isalnum())
+        l = 0
+        r = len(concat)-1
+        while l < r:
+            if concat[l] != concat[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
